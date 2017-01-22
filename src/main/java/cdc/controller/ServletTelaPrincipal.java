83f5e4b77befbc77b368ed43ebdd3bbbfa4b3bbd
Julio Cesar Alcântara Lopes <julio.cal93@gmail.com>
2017-01-22 14:26:50 -0200

    Novas implementações a certo da compra.

---
+++ src/main/java/cdc/controller/ServletTelaPrincipal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cdc.controller;
+
+import cdc.model.Produto;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author cesar
+ */
+@WebServlet("/TelaPrincipal")
+public class ServletTelaPrincipal extends HttpServlet {
+    //private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idProduto = request.getParameter("idProduto");
+        System.out.println("id do Produto: " + idProduto);
+        
+        Produto p1 = new Produto();
+        p1.setIdProduto(Integer.parseInt(idProduto));
+        request.setAttribute("produto1", p1);
+        request.getRequestDispatcher("/MostraProdutoCompra.jsp").forward(request, response);
+    }
+   
+}
